5e7934d79329c17ef76c22af9f99a48410becc1f
Yuki Akiyama <you2197901@gmail.com>
2015-12-03 00:54:28 +0900

    Create ex4.js

---
+++ example/ex4.js
@@ -0,0 +1,41 @@
+"use strict"
+const BinaryArray = require("binaryarray")
+
+const JOB = Object.freeze({
+    NONE : 0,
+    FIGHTER : 1,
+    MAGE : 2,
+    PRIEST : 3,
+    MONK : 4,
+    KNIGHT : 5,
+    SUMMONER : 6,
+});
+
+const JOB_MAX = Math.max.apply(Math, Object.keys(JOB).map(k => JOB[k] )) + 1;
+
+const JOB_GROUP = Object.freeze({
+    TANK : BinaryArray.deserialize({
+        FIGHTER : 1,
+        MONK : 1,
+        KNIGHT : 1,
+    }, JOB, JOB_MAX),
+    CASTER : BinaryArray.deserialize({
+        MAGE : 1,
+        PRIEST : 1,
+        SUMMONER : 1,
+    }, JOB, JOB_MAX),
+    HEALER : BinaryArray.deserialize({
+        PRIEST : 1,
+    }, JOB, JOB_MAX),
+    EXJOB : BinaryArray.deserialize({
+        SUMMONER : 1,
+        KNIGHT : 1,
+    }, JOB, JOB_MAX),
+})
+
+let job_id = JOB.FIGHTER;
+if( JOB_GROUP.TANK.check([job_id],[]) ){
+    console.log("tank job")
+}else{
+    console.log("not tank job")
+}
